cc5ca7ed875ff80a61297b9981df61fd84c215bf
Mudith Daga <dagamudith@gmail.com>
2022-07-16 22:57:47 +0530

    Update files

--- 2020/4.py
+++ 2020/4.py
@@ -1,7 +1,7 @@
 # If you already have the data as an input use:
 data = [] # Your Advent Input
 # However, it is highly recommended to run this:
-location = "C:\\Users\\dagam\\Downloads\\input.txt" # Path of the input.txt provided from Advent Calendar
+location = "" # Path of the input.txt provided from Advent Calendar
 try:
     with open(location,'r') as f:
         data = (''.join(f.readlines())).split('\n\n')
---
+++ 2020/5.py
@@ -0,0 +1,13 @@
+# If you already have the data as an input use:
+data = [] # Your Advent Input
+# However, it is highly recommended to run this:
+location = "" # Path of the input.txt provided from Advent Calendar
+try:
+    with open(location,'r') as f:
+        data = f.readlines()
+        f.close()
+except:
+    pass
+data = [o.rstrip('\n') for o in data]
+if data[-1] == "'+":
+    del data[-1]
