3e81181e364f850de0f865fc73d9f62520f23cbc
Mudith Daga <dagamudith@gmail.com>
2022-07-16 23:08:18 +0530

    Update 5.py

--- 2020/5.py
+++ 2020/5.py
@@ -1,7 +1,7 @@
 # If you already have the data as an input use:
 data = [] # Your Advent Input
 # However, it is highly recommended to run this:
-location = "" # Path of the input.txt provided from Advent Calendar
+location = "C:\\Users\\dagam\\Downloads\\input.txt" # Path of the input.txt provided from Advent Calendar
 try:
     with open(location,'r') as f:
         data = f.readlines()
@@ -11,3 +11,13 @@ except:
 data = [o.rstrip('\n') for o in data]
 if data[-1] == "'+":
     del data[-1]
+
+def get_row_column(string):
+    row_pos = string[:8]
+    print(row_pos)
+def day5(part=1):
+    if part not in [1,2]:
+        part = 1
+    if part == 1:
+        for i in data:
+            get_row_column(i)
