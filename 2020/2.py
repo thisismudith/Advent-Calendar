09189cd551119193271c4c06afd5125869d9b13d
Mudith Daga <dagamudith@gmail.com>
2022-07-19 17:07:33 +0530

    Update 2.py

--- 2020/2.py
+++ 2020/2.py
@@ -1,7 +1,7 @@
 # If you already have the data as an input use:
 data = [] # Your Advent Calendar Input
 # However, it is highly recommended to run this:
-location = "" # Path of the input.txt provided from Advent Calendar (Replace '\' → '\\')
+location = "C:\\Users\\dagam\\Downloads\\input.txt" # Path of the input.txt provided from Advent Calendar (Replace '\' → '\\')
 try:
     with open(location,'r') as f:
         data = f.readlines()
@@ -27,5 +27,5 @@ def day2(part=1):
             if (pwd[start-1] == letter and pwd[end-1] != letter) or (pwd[start-1] != letter and pwd[end-1] == letter):
                 count += 1
     return count
-print('Part 1: '+day2(part=1))
-print('Part 2: '+day2(part=2))
+print('Part 1: '+str(day2(part=1)))
+print('Part 2: '+str(day2(part=2)))
